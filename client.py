73aa3e05eb3a4f6b9442a0bb78b5e83754aaac42
Oliver Heineman <oliver.heineman@gmail.com>
2025-10-19 12:24:38 +0200

    Vet inte om det funkar

--- client.py
+++ client.py
@@ -1,9 +1,23 @@
+# client.py
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP socket
-client.connect(('localhost', 8080))  # Connect to the server
+server_ip = input("Enter the servers IP adress: ").strip()
+server_port = 9090
 
-client.send('Hello from client!'.encode())  # Send data to the server¨
-print("TRIED SENDING REQUEST")
-print(client.recv(1024).decode())  # Receive a response from the server
-print("TRIED RECIVING REQUEST")
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a TCP socket
+
+try:
+    client.connect((server_ip, server_port))  # Connect to the server. CMD -> ipconfig -> change localhost to ip
+    print(f"Connected to server at {server_ip}:{server_port}")
+
+    client.send('Hello from client!'.encode())  # Send data to the server¨
+    print("Sent message to server.")
+
+    response = client.recv(1024).decode() # Receive a response from the server
+    print(f"Server says: {response}")
+
+except Exception as e:
+    print(f"Connection failed: {e}")
+
+finally:
+    client.close()
--- server.py
+++ server.py
@@ -1,13 +1,38 @@
+# server.py
 import socket
 
+def get_local_ip():
+    """Finds the local ip adress on this machine (LAN adress)."""
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    try:
+        s.connect(("8.8.8.8", 80))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = "127.0.0.1"
+    finally:
+        s.close
+    return ip
+
+
+server_ip = get_local_ip()
+server_port = 9090
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)# Create a TCP socket
-server.bind(('localhost', 8080))# Bind the socket to an address and port
+# server.bind(('localhost', 8080))# Bind the socket to an address and port
+server.bind((server_ip, server_port))
 
 server.listen(5)# Listen for incoming connections
 
 while True:
     client, addr = server.accept()# Accept a connection
-    print(client.recv(1024).decode())# Print the client response
-    print("TRIED RECIVING RESPONSE")
+    print(f"Connection from {addr}")
+
+    message = client.recv(1024).decode()
+    print(f"Client says: {message}")# Print the client response
+
+    # print("TRIED RECIVING RESPONSE")
     client.send('Hello from server!'.encode())# Send a response to the client
-    print("TRIED SENDING RESPONSE")
+    print("Sent response to client\n")
+
+    client.close()
